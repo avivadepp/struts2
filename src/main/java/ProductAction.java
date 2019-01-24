f8eab4dc5bc84ba48e43d93b9c963f7030337e9d
avivadepp <avivadepp@163.com>
2019-01-24 17:58:03 +0800

    获取request和response对象

--- src/main/java/ProductAction.java
+++ src/main/java/ProductAction.java
@@ -1,3 +1,11 @@
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 /**
  * @program: struts2
  * @description: ProductAction用于控制
@@ -5,7 +13,7 @@
  * @rewrite: Aviva Wang
  * @create: 2019-01-24-41
  **/
-public class ProductAction {
+/*public class ProductAction {
     private Product product;
 
     public String show() {
@@ -18,6 +26,43 @@ public class ProductAction {
         System.out.println("product.name:"+product.getName());
         return "show";
     }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}*/
+
+public class ProductAction extends ActionSupport {
+    private Product product;
+
+    public String show() {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpServletResponse response = ServletActionContext.getResponse();
+
+        System.out.println("request:\t" + request);
+        System.out.println("response:\t" + response);
+
+        product = new Product();
+        product.setName("iphone7");
+        return "show";
+    }
+
+    public String add() {
+
+        Map m = ActionContext.getContext().getSession();
+        m.put("name", product.getName());
+        return "show";
+    }
+
+    // public void validate(){
+    // if ( product.getName().length() == 0 ){
+    // addFieldError( "product.name", "name can't be empty" );
+    // }
+    // }
 
     public Product getProduct() {
         return product;

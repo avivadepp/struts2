b8b55ee73c8c52b38256534e39f0940bc3206226
avivadepp <avivadepp@163.com>
2019-01-30 13:53:13 +0800

    mybatis

---
+++ src/java/com/ssh/Main/TestMybatis.java
@@ -0,0 +1,33 @@
+package com.ssh.Main;
+
+import com.ssh.model.Category;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @program: struts2
+ * @description:
+ * @autor:
+ * @rewrite: Aviva Wang
+ * @create: 2019-01-30-30
+ **/
+public class TestMybatis {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session=sqlSessionFactory.openSession();
+
+        List<Category> cs=session.selectList("listCategory");
+        for (Category c : cs) {
+            System.out.println(c.getName());
+        }
+
+    }
+}
---
+++ src/java/com/ssh/model/Category.java
@@ -0,0 +1,25 @@
+package com.ssh.model;
+
+/**
+ * @program: struts2
+ * @description:
+ * @autor:
+ * @rewrite: Aviva Wang
+ * @create: 2019-01-30-08
+ **/
+public class Category {
+    private int id;
+    private String name;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
